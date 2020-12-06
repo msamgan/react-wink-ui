3c71f0c5175c18ab9a4a82e7c3af0d53eb7a30b4
msamgan <mohdsamgankhan@gmail.com>
2020-12-07 01:53:41 +0400

    pages rendered from api

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 const Header = () => {
+    const [pages, setPages] = useState([])
+
+    useEffect(() => {
+        fetchPages()
+            .then()
+    }, [])
+
+    const fetchPages = async () => {
+        await fetch('http://localhost:5000/page/listing')
+            .then(response => response.json())
+            .then(result => setPages(result.package))
+    }
+
     return (
         <header className="blog-header py-3">
             <div className="row flex-nowrap justify-content-between align-items-center">
                 <div className="col-4">
-                    <a className="blog-header-logo text-dark" href="https://codebysamgan.com">Samgan's Journal</a>
+                    <a className="blog-header-logo text-dark" href="/">Samgan's Journal</a>
                 </div>
                 <div className="col-8 d-flex justify-content-end align-items-center">
-                    <a className="ml-4" href="https://codebysamgan.com/page/about">ABOUT</a>
-                    <a className="ml-4" href="https://codebysamgan.com/page/portfolio">PORTFOLIO</a>
-                    <a className="ml-4" href="https://codebysamgan.com/page/tv-series-i-watched">TV SERIES I
-                        WATCHED</a>
-                    <a className="ml-4" href="https://codebysamgan.com/page/uses">USES</a>
+                    {pages.map(page =>
+                        <Link to={`/page/${page.slug}`} className="ml-4">{page.title}</Link>
+                    )}
                 </div>
             </div>
         </header>
